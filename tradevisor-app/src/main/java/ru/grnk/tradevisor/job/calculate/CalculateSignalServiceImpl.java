a97b3002db96e9f04e099e19c59d085dba504d15
18950416 <segrebenyuk@sberbank.ru>
2023-07-06 13:46:08 +0300

    trv-3. calculate strategy.first step.

--- tradevisor-app/src/main/java/ru/grnk/tradevisor/job/calculate/CalculateSignalServiceImpl.java
+++ tradevisor-app/src/main/java/ru/grnk/tradevisor/job/calculate/CalculateSignalServiceImpl.java
@@ -5,15 +5,26 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.grnk.tradevisor.job.JobRunnerService;
 import ru.grnk.tradevisor.metrics.Jobs;
+import ru.grnk.tradevisor.repository.TickersRepository;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class CalculateSignalServiceImpl implements JobRunnerService {
 
+    private final TickersRepository tickersRepository;
+
     @Override
     public void doWork() {
         log.info("calculate all signals mf");
+        var tickers = tickersRepository.getAllTickers();
+        if (tickers.isEmpty()) {
+            log.info("нет тикеров ждемс когда появятся");
+            return;
+        }
+        tickers.stream().forEach(x -> {
+
+        });
     }
 
     @Override
