205525b5088d408fc05d6407acd68e24db12a2f4
Tatiana Butina <hurryupson0@gmail.com>
2025-10-29 00:09:10 +0300

    draw lines tp, sl, po

--- tradevisor-app/src/main/java/ru/grnk/tradevisor/notify/plot/quickchart/ChartDto.java
+++ tradevisor-app/src/main/java/ru/grnk/tradevisor/notify/plot/quickchart/ChartDto.java
@@ -18,13 +18,14 @@ public class ChartDto {
     private String type;
     private ChartData data;
     private ChartOptions options;
-    private List<ChartPlugins> plugins;
 
-    @Builder public static record ChartData(
+    @Builder
+    public static record ChartData(
             List<String> labels,
             List<ChartDataset> datasets
     ) {
-        @Builder public static record ChartDataset(
+        @Builder
+        public static record ChartDataset(
                 String label,
                 String yAxisID,
                 List<Dataset> data,
@@ -35,16 +36,25 @@ public class ChartDto {
                 Integer borderWidth,
                 Integer pointRadius
         ) {
-            @Builder public static  record Dataset(long x, Float y, Float o, Float h, Float l, Float c){}
-            @Builder public static record OhlcColor(String up, String down, String unchanged) {}
+            @Builder
+            public static record Dataset(long x, Float y, Float o, Float h, Float l, Float c) {
+            }
+
+            @Builder
+            public static record OhlcColor(String up, String down, String unchanged) {
+            }
         }
     }
-    @Builder public static record ChartOptions(
+
+    @Builder
+    public static record ChartOptions(
             Boolean responsive,
             Boolean maintainAspectRatio,
-            ChartScales scales
+            ChartScales scales,
+            ChartPlugins plugins
     ) {
-        @Builder public static record ChartScales(
+        @Builder
+        public static record ChartScales(
                 ChartOptionsScale x,
                 ChartOptionsScale y1,
                 ChartOptionsScale y2,
@@ -57,7 +67,8 @@ public class ChartDto {
                 ChartOptionsScale y9,
                 ChartOptionsScale y10
         ) {
-            @Builder public static record ChartOptionsScale(
+            @Builder
+            public static record ChartOptionsScale(
                     ChartAdapters adapters,
                     ChartTime time,
                     String stack,
@@ -65,20 +76,71 @@ public class ChartDto {
                     Integer weight,
                     Boolean display
             ) {
-                @Builder public static record ChartAdapters(ChartZone date) {
-                    @Builder public static record ChartZone(String zone) { }
+                @Builder
+                public static record ChartAdapters(ChartZone date) {
+                    @Builder
+                    public static record ChartZone(String zone) {
+                    }
                 }
-                @Builder public static record ChartTime(String unit, Integer stepSize, DisplayFormats displayFormats) {
-                    @Builder public static record DisplayFormats (String day, String month){}
+
+                @Builder
+                public static record ChartTime(String unit, Integer stepSize, DisplayFormats displayFormats) {
+                    @Builder
+                    public static record DisplayFormats(String day, String month) {
+                    }
                 }
             }
         }
-    }
-    @Builder public static record ChartPlugins(Legend legend, Title title, ChartAnnotation annotation) {
-        @Builder public static record Legend (Boolean display) {}
-        @Builder public static record Title (Boolean display, String text, Font font) {
-            @Builder public static record Font (Integer size) {}
+
+        @Builder
+        public static record ChartPlugins(Legend legend, Title title, ChartAnnotation annotation) {
+            @Builder
+            public static record Legend(Boolean display) {
+            }
+
+            @Builder
+            public static record Title(Boolean display, String text, Font font) {
+                @Builder
+                public static record Font(Integer size) {
+                }
+            }
+
+            @Builder
+            public static record ChartAnnotation(List<ChartAnnotationItem> annotations) {
+                @Builder
+                public static record ChartAnnotationItem(
+                        String type,
+                        String mode,
+                        Float yMin,
+                        Float yMax,
+                        Long xMax,
+                        Long xMin,
+                        String borderColor,
+                        Label label,
+                        ArrowHeads arrowHeads
+                ) {
+                    @Builder
+                    public static record Label(
+                            Boolean enabled,
+                            String color,
+                            String backgroundColor,
+                            String content,
+                            String position,
+                            Padding padding
+                    ) {
+                        @Builder
+                        public static record Padding(Integer bottom) {
+                        }
+                    }
+
+                    @Builder
+                    public static record ArrowHeads(Start start) {
+                        @Builder
+                        public static record Start(Boolean enabled) {
+                        }
+                    }
+                }
+            }
         }
-        @Builder public static record ChartAnnotation(Map<String, Object> annotations) {}
     }
 }
