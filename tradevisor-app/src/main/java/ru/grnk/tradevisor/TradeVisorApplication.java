c0cba61305d4dae0935bb6f9f0f37e1a3a2aaf45
Tatiana Butina <hurryupson0@gmail.com>
2025-12-11 09:41:20 +0300

    rootcode changes.4.

--- tradevisor-app/src/main/java/ru/grnk/tradevisor/TradeVisorApplication.java
+++ tradevisor-app/src/main/java/ru/grnk/tradevisor/TradeVisorApplication.java
@@ -4,7 +4,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import ru.grnk.tradevisor.common.properties.TradevisorProperties;
 
 
