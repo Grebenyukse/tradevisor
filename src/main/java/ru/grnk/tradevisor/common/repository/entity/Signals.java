d032613fa1530e43c2057034c497f55dede82c25
Tatiana Butina <hurryupson0@gmail.com>
2025-12-14 13:12:06 +0300

    all repositories in one place.4

---
+++ src/main/java/ru/grnk/tradevisor/common/repository/entity/Signals.java
@@ -0,0 +1,58 @@
+package ru.grnk.tradevisor.common.repository.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import ru.grnk.tradevisor.notify.plot.dto.ChartLineDto;
+
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Builder
+@Setter
+@Getter
+@Entity
+@Table(name = "signals", schema = "tradevisor")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Signals implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "ticker_code")
+    private String tickerCode;
+
+    @Column(name = "direction")
+    private Short direction;
+
+    @Column(name = "price_open")
+    private Float priceOpen;
+
+    @Column(name = "stop_loss")
+    private Float stopLoss;
+
+    @Column(name = "take_profit")
+    private Float takeProfit;
+
+    @Column(name = "created_at")
+    private OffsetDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private OffsetDateTime updatedAt;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "strategy_props", columnDefinition = "jsonb")
+    @Convert(converter = ChartLineListToJsonbConverter.class)
+    private List<ChartLineDto> strategyProps;
+
+}
---
+++ src/main/java/ru/grnk/tradevisor/common/repository/entity/Tickers.java
@@ -0,0 +1,65 @@
+package ru.grnk.tradevisor.common.repository.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Builder
+@Setter
+@Getter
+@Entity
+@Table(name = "tickers", schema = "tradevisor")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tickers implements Serializable {
+
+    @Id
+    @Column(name = "ticker_code")
+    private String tickerCode;
+
+    @Column(name = "ticker", nullable = false)
+    private String ticker;
+
+    @Column(name = "figi", nullable = false)
+    private String figi;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "market_type")
+    private String marketType;
+
+    @Column(name = "exchange")
+    private String exchange;
+
+    @Column(name = "precision")
+    private Integer precision;
+
+    @Column(name = "lot")
+    private Integer lot;
+
+    @Column(name = "go")
+    private Integer go;
+
+    @Column(name = "expiration")
+    private LocalDateTime expiration;
+
+    @Column(name = "currency")
+    private String currency;
+
+    @Column(name = "provider")
+    private String provider;
+
+    @Column(name = "status", length = 100)
+    private String status;
+
+    @Column(name = "load_priority")
+    private Integer loadPriority;
+
+    @Column(name = "trade_ticker_code")
+    private String tradeTickerCode;
+
+
+}
