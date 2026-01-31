e291b6b17c41affaa460545918d7c3724021a265
Tatiana Butina <hurryupson0@gmail.com>
2026-01-31 15:35:14 +0300

    deploy config. 44.

--- src/main/resources/changelog/01.000.00/2023-05-20-trv-2-add-market-data.sql
+++ src/main/resources/changelog/01.000.00/2023-05-20-trv-2-add-market-data.sql
@@ -1,8 +1,7 @@
 --liquibase formatted sql
 
 --changeset SE-Grebenyuk:01.000.00/TVR-1/init-schema
---rollback drop table tradevisor.market_data
--- rollback drop table tradevisor.market_data
+--rollback drop table if exists tradevisor.market_data;
 create table if not exists tradevisor.market_data
 (
     ticker_code  varchar   not null references tradevisor.tickers (ticker_code),
@@ -10,8 +9,6 @@ create table if not exists tradevisor.market_data
     open   real      null,
     high   real      null,
     low    real      null,
-    close  real      null
+    close  real      null,
+    constraint market_data_time_ticker_code_pk primary key (time, ticker_code)
 );
-alter table tradevisor.market_data drop constraint if exists market_data_time_ticker_code_pk;
-alter table tradevisor.market_data add constraint market_data_time_ticker_code_pk primary key (time, ticker_code);
-
