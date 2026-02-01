fd7f5bac6a827f7a148e1ec22281ff42980905cb
Tatiana Butina <hurryupson0@gmail.com>
2026-02-02 00:58:11 +0300

    deploy config. 52.

--- src/test/java/ru/grnk/tradevisor/integration/BaseIntegrationTest.java
+++ src/test/java/ru/grnk/tradevisor/integration/BaseIntegrationTest.java
@@ -93,7 +93,7 @@ public abstract class BaseIntegrationTest {
 
     @BeforeEach
     public void logProperties() {
-        log.info("properties: {}", properties.toString());
+//        log.info("properties: {}", properties.toString());
     }
 
     @AfterAll
