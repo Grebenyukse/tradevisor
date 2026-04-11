e418b41516063127877e20661b0a211ea62370c6
Tatiana Butina <hurryupson0@gmail.com>
2026-04-11 20:38:55 +0300

    small refactor.2.

--- src/main/java/ru/grnk/tradevisor/cleanup/SignalCleanupService.java
+++ src/main/java/ru/grnk/tradevisor/cleanup/SignalCleanupService.java
@@ -6,11 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import ru.grnk.tradevisor.common.log.CleanUpLogsService;
 import ru.grnk.tradevisor.common.properties.TradevisorProperties;
 import ru.grnk.tradevisor.common.repository.SignalsRepository;
-
-import java.time.LocalDateTime;
 
 @Slf4j
 @Service
@@ -20,7 +17,6 @@ public class SignalCleanupService {
 
     private final SignalsRepository signalsRepository;
     private final TradevisorProperties tradevisorProperties;
-    private final CleanUpLogsService cleanUpLogsService;
 
     @Scheduled(cron = "${app.cleanup.cron}")
     public void cleanup() {
@@ -28,6 +24,5 @@ public class SignalCleanupService {
         var retention = tradevisorProperties.cleanup().retentionDays();
         var res = signalsRepository.expirePublishedSignals(retention);
         log.info("переведено в статус EXPIRED {} сигналов. retention: {}", res, retention);
-        cleanUpLogsService.deleteByLogDateBefore(LocalDateTime.now().minusDays(30));
     }
 }
