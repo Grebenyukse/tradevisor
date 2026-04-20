ffc017b01384b4e18776b2d03036fd447b314082
Tatiana Butina <hurryupson0@gmail.com>
2026-04-20 18:01:20 +0300

    small refactor.16.

--- src/main/java/ru/grnk/tradevisor/integration/bybit/BybitTradeClientImpl.java
+++ src/main/java/ru/grnk/tradevisor/integration/bybit/BybitTradeClientImpl.java
@@ -123,7 +123,7 @@ public class BybitTradeClientImpl implements TradeClient {
     @SneakyThrows
     @Override
     public boolean openPosition(Signals signal) {
-        log.info("попытка открыть позицию для сигнала: {}", signal);
+        log.info("Это попытка открыть позицию для сигнала: {}", signal);
         Tickers spotTicker = tickersRepository.getTickerByTickerCode(signal.getTickerCode());
         Tickers tradeTicker = tickersRepository.findTradeTickerByTickerCodeIfExists(spotTicker.getTickerCode())
                 .orElse(spotTicker);
